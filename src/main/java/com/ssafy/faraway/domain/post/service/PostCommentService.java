22b0c1b7e6e8f8dd8d0a1657feb626798aee4b9b
longBright <longbright0804@gmail.com>
2023-05-22 14:58:28 +0900

    FAR-158/refactor: 게시글 리팩토링

--- src/main/java/com/ssafy/faraway/domain/post/service/PostCommentService.java
+++ src/main/java/com/ssafy/faraway/domain/post/service/PostCommentService.java
@@ -1,12 +1,12 @@
 package com.ssafy.faraway.domain.post.service;
 
-import com.ssafy.faraway.domain.post.controller.dto.req.SavePostCommentRequest;
+import com.ssafy.faraway.domain.post.service.dto.SavePostCommentDto;
 import com.ssafy.faraway.domain.post.service.dto.UpdatePostCommentDto;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface PostCommentService {
-    Long save(Long postId, Long memberId, SavePostCommentRequest request);
+    Long save(Long postId, Long memberId, SavePostCommentDto dto);
 
     Long update(Long commentId, UpdatePostCommentDto dto);
 
