35b0f12d51e588fe1d54f6dff42c8b7e076325a1
longBright <longbright0804@gmail.com>
2023-05-22 14:48:51 +0900

    hotfix: CORS Configuration

--- src/main/java/com/ssafy/faraway/common/config/WebConfig.java
+++ src/main/java/com/ssafy/faraway/common/config/WebConfig.java
@@ -53,6 +53,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*");
     }
 }
